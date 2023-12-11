ea5519ed08379cda16c02664c0812b6f1856e07e
Praveen Kumar <praveen.adrosonic@gmail.com>
2023-12-11 13:00:05 +0530

    few changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,9 +3,44 @@ import React, { Component } from 'react'
 export default class NavBar extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="Navbar.tsx">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="NavBar.tsxnavbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="NavBar.tsx">Home</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="NavBar.tsx">Link</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="NavBar.tsx" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul className="dropdown-menu">
+                        <li><a className="dropdown-item" href="NavBar.tsx">Action</a></li>
+                        <li><a className="dropdown-item" href="NavBar.tsx">Another action</a></li>
+                        <li><hr className="dropdown-divider"/></li>
+                        <li><a className="dropdown-item" href="NavBar.tsx">Something else here</a></li>
+                    </ul>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link disabled" aria-disabled="true" href='NavBar.tsx'>Disabled</a>
+                    </li>
+                </ul>
+                <form className="d-flex" role="search">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button className="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                </div>
+            </div>
+        </nav>
+      </>
     )
   }
 }
